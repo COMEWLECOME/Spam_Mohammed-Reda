fec8cbda544ea1a78e5f4968556a65b7ba1eb7d3
COMEWELCOME <mohasoft@gmail.com>
2024-01-19 14:23:15 +0100

    code

--- text_streamlit.py
+++ text_streamlit.py
@@ -8,17 +8,21 @@ data['len']=data['mail'].str.len()
 data['nombre_mots']=data['mail'].str.split().str.len()
 # st.sidebar → colonne de gauche
 st.sidebar.title('Configuration')
+
+n2 = st.sidebar.radio('choisis le type de mail',['spam','ham'])
+
 nl = st.sidebar.slider('Lignes', 
     min_value=0, 
     max_value=min(50,data.shape[0]))
 
-n2 = st.sidebar.radio('choisis le type de mail',['spam','ham'])
+df=data[data['type']==n2].iloc[0:nl]
+
 
 # partie centrale
-st.table(data[data['type']==n2].iloc[0:nl])
+st.table(df)
 
 # Bar graph
-st.bar_chart(data=data.iloc[0:nl], y='len')
+st.bar_chart(data=df, y='len')
 
 # Scatter plot
 st.scatter_chart(data=data.iloc[:nl], x='len', y='nombre_mots')
