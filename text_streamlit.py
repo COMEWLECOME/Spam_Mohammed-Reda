bbb7c0153bc27211518e39585b3931dcf621c69e
COMEWELCOME <mohasoft@gmail.com>
2024-01-19 11:36:34 +0100

    avec l'exemple streamlit

---
+++ text_streamlit.py
@@ -0,0 +1,18 @@
+#python3 -m streamlit run text_streamlit.py 
+import streamlit as st
+import pandas as pd
+url = "SMSSpamCollection.txt"
+data=pd.read_csv(url, sep='\t', header=None )
+data.rename(columns={0:'type',1:'mail'}, inplace=True)
+data['len']=data['mail'].str.len()
+data['nombre_mots']=data['mail'].str.split().str.len()
+# st.sidebar → colonne de gauche
+st.sidebar.title('Configuration')
+nl = st.sidebar.slider('Lignes', 
+    min_value=0, 
+    max_value=min(50,data.shape[0]))
+
+n2 = st.sidebar.radio('choisis le type de mail',['spam','ham'])
+
+# partie centrale
+st.table(data.iloc[0:nl])
